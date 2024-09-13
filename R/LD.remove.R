a2cbcb56651e788b3f4c273d7092f1f0c5b31acb
fangjun-xu <154345273+fangjun-xu@users.noreply.github.com>
2024-09-13 20:28:39 +0800

    Update LD.remove.R

--- R/LD.remove.R
+++ R/LD.remove.R
@@ -19,49 +19,59 @@ LD.remove <- function(index = NULL, geno = NULL, value = NULL,
   if (length(index) < 2) {
     return(index)
   }else {
-    if (verbose) {
-      cat("LD removing...\n")
-      pb <- pbapply::timerProgressBar(width = 30, char = "-", style = 3)
-      on.exit(close(pb))
-    }
     ldr <- function(index, value) {
       X1 <- geno[index, ]
       X1<-sweep(X1[, ], 1, rowMeans(X1))
-		  rownames(X1) <- index
-		  sigma <- tcrossprod(X1) / (dim(geno)[2] - 1)
-		  sigma.distance <- stats::as.dist(1 - abs(stats::cov2cor(sigma)))
-		  fit <- stats::hclust(sigma.distance, method = "single")
-		  clusters <- stats::cutree(fit, h = 1 - LD.threshold)
-		  names(value) <- rownames(X1)
-		  top.selected <- sapply(1:max(clusters), function(i) {
-			  cluster_elements <- clusters == i
-			  top_within <- which.min(value[cluster_elements])
-			  if (length(top_within) == 0) top_within <- 1
-			  return(which(cluster_elements)[top_within])
-		  })
-		  index<-as.numeric(names(top.selected))
-		  rm(sigma, sigma.distance, fit, clusters, top.selected)
+      rownames(X1) <- index
+      sigma <- tcrossprod(X1) / (dim(geno)[2] - 1)
+      sigma.distance <- stats::as.dist(1 - abs(stats::cov2cor(sigma)))
+      fit <- stats::hclust(sigma.distance, method = "single")
+      clusters <- stats::cutree(fit, h = 1 - LD.threshold)
+      names(value) <- rownames(X1)
+      top.selected <- sapply(1:max(clusters), function(i) {
+	cluster_elements <- clusters == i
+	top_within <- which.min(value[cluster_elements])
+	if (length(top_within) == 0) top_within <- 1
+	return(which(cluster_elements)[top_within])
+      })
+      index<-as.numeric(names(top.selected))
+      rm(sigma, sigma.distance, fit, clusters, top.selected)
       return(index)
     }
     if (length(index) <= 10000) {
+      if (verbose) {
+        cat("LD removing...\n")
+        pb <- pbapply::timerProgressBar(width = 30, char = "-", style = 3)
+        on.exit(close(pb))
+      }
       index <- ldr(index = index, value = value)
+      if (verbose) {
+        pbapply::setTimerProgressBar(pb, 1)
+      }
     }else {
       nbreak <- ceiling(length(index) / 10000)
       cutind <- cut(1:length(index), breaks = nbreak, labels = FALSE)
+      if (verbose) {
+        cat("LD removing...\n")
+        pb <- pbapply::timerProgressBar(max = nbreak + 1, width = 30, char = "-", style = 3)
+        on.exit(close(pb))
+      }
       indi <- lapply(1:nbreak, function(i) {
-        idi <- index[which(cutind == 1, arr.ind = TRUE)]
-        vi <- value[which(cutind == 1, arr.ind = TRUE)]
+        idi <- index[which(cutind == i, arr.ind = TRUE)]
+        vi <- value[which(cutind == i, arr.ind = TRUE)]
+	if (verbose) {
+          pbapply::setTimerProgressBar(pb, i)
+        }
         return(ldr(index = idi, value = vi))
       })
       indi <- unlist(indi)
       vali <- value[which(index %in% indi)]
       index <- ldr(index = indi, value = vali)
-    }
-
-		if (verbose) {
-      pbapply::setTimerProgressBar(pb, 1)
-    }
-		return(index)
-    }
+      if (verbose) {
+        pbapply::setTimerProgressBar(pb, nbreak + 1)
+      }
+    }	
+    return(index)
+  }
 }
 
